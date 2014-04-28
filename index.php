68265f3dc6e2b44631d5e45543e8527e619f11ca
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-04-28 08:04:40 +0100

    Base plugin

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Filesize report
+ *
+ * @package    report
+ * @subpackage filesize
+ * @copyright  2014 University of Kent
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require(dirname(__FILE__) . '/../../config.php');
+require_once($CFG->libdir . '/adminlib.php');
+
+admin_externalpage_setup('reportfilesize', '', null, '', array('pagelayout' => 'report'));
+
+$page    = optional_param('page', 0, PARAM_INT);
+$perpage = optional_param('perpage', 25, PARAM_INT);
+$category = optional_param('category', 0, PARAM_INT);
+
+
+echo $OUTPUT->header();
+
+echo $OUTPUT->heading(get_string('pluginname', 'report_filesize'));
+
+
+echo $OUTPUT->footer();
